f24e90d28045f9801fef7601218eade9666fd6d3
Jessamyn Smith <geekchick77@gmail.com>
2015-05-08 07:32:03 -0400

    Added server capabilities to deploy to heroku.

---
+++ server.js
@@ -0,0 +1,26 @@
+require('newrelic');
+
+var sslRedirect = require('heroku-ssl-redirect'),
+    express = require('express'),
+    app = express();
+
+// enable ssl redirect
+app.use(sslRedirect());
+
+app.use(express.static('www'));
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+// API Routes
+// app.get('/blah', routeHandler);
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
